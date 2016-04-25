67730694642202e1d5696db653fa86dd6aeed102
StFreve <Golfik2007@gmail.com>
2016-04-25 15:11:12 +0400

    Update 25.04.2016

--- Client/Client/Task.h
+++ Client/Client/Task.h
@@ -1,30 +1,30 @@
 #pragma once
 #include "EXider.h"
 namespace EXider {
-	class Task {
-		boost::asio::io_service& m_io;
+    class Task {
+        boost::asio::io_service& m_io;
         mutable boost::recursive_mutex m_mutexForResult;                                // Mutex to read or write resulting information
-		
-		const std::string m_taskName;
-		const size_t m_tID;
-		PCList m_workingPCs;                                                            // PC's working on the task
-		std::vector<std::string> m_result;                                              // Results received from RemotePC's
 
-		const std::string m_downloadURL, m_executeCommand;                              // Download URL (ftp) and Executeing Command for RemotePC
-		bool m_downloadsFiles;
-		bool m_autoFree;                                                                // Automaticly delete PC from the task, when result is received
-		
-		void handler( boost::shared_ptr<RemotePC> fromPC, std::string result );	        // call-back function for RemotePC
+        const std::string m_taskName;
+        const size_t m_tID;
+        PCList m_workingPCs;                                                            // PC's working on the task
+        std::vector<std::string> m_result;                                              // Results received from RemotePC's
 
-		boost::thread m_thread;
-	public:
-		Task(boost::asio::io_service& io, const PCList& workingPCs, const std::string downloadURL, const std::string executeCommand, const std::string& taskName, int tID, bool autoFree = 0 );
-		Task( boost::asio::io_service& io, const PCList& workingPCs, const std::string executeCommand, const std::string& taskName, int tID, bool autoFree = 0 );
-		void run();
-		void stop();
-		const std::string getResult(const std::string& delimeter = " ") const;
-		const std::string getInfromation() const;
-		const std::string& getName() const;
-		const size_t getID() const;
-	};
+        const std::string m_downloadURL, m_executeCommand;                              // Download URL (ftp) and Executeing Command for RemotePC
+        bool m_downloadsFiles;
+        bool m_autoFree;                                                                // Automaticly delete PC from the task, when result is received
+
+        void handler( boost::shared_ptr<RemotePC> fromPC, std::string result );	        // call-back function for RemotePC
+
+        boost::thread m_thread;
+    public:
+        Task( boost::asio::io_service& io, const PCList& workingPCs, const std::string downloadURL, const std::string executeCommand, const std::string& taskName, int tID, bool autoFree = 0 );
+        Task( boost::asio::io_service& io, const PCList& workingPCs, const std::string executeCommand, const std::string& taskName, int tID, bool autoFree = 0 );
+        void run();
+        void stop();
+        const std::string getResult( const std::string& delimeter = " " ) const;
+        const std::string getInfromation() const;
+        const std::string& getName() const;
+        const size_t getID() const;
+    };
 }
